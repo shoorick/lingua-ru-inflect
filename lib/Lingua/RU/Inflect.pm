91d76189abb898c35db5b8a25159895188280198
as <as@058c6afd-2165-45e8-b96a-abfe060194d7>
2010-01-24 20:20:24 +0000

    Создана традиционная для Subversion структура каталогов

---
+++ lib/Lingua/RU/Inflect.pm
@@ -0,0 +1,110 @@
+package Lingua::RU::Inflect;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Lingua::RU::Inflect - The great new Lingua::RU::Inflect!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Lingua::RU::Inflect;
+
+    my $foo = Lingua::RU::Inflect->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Alexander Sapozhnikov, C<< <shoorick at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-lingua-ru-inflect at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Lingua-RU-Inflect>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Lingua::RU::Inflect
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Lingua-RU-Inflect>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Lingua-RU-Inflect>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Lingua-RU-Inflect>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Lingua-RU-Inflect/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2010 Alexander Sapozhnikov.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of Lingua::RU::Inflect
